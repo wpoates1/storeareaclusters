6e74084fd75f9ca1cee2c25eabdfeba08a5d7559
Bill Oates <wpoates1@users.noreply.github.com>
2023-05-16 17:03:34 +0100

    Update 210 Add Geography to PVT_CONFLATED.sql

--- phase2/210 Add Geography to PVT_CONFLATED.sql
+++ phase2/210 Add Geography to PVT_CONFLATED.sql
@@ -5,13 +5,13 @@ ALTER TABLE `_PROJECT_._DATASET_.PVT_Conflated`
     ADD COLUMN IF NOT EXISTS centroid GEOGRAPHY;
 
 -- Step 2: Update the column values in PVT_Conflated table
-UPDATE `gdms-demo-20230201.census2021.PVT_Conflated` A
+UPDATE `_PROJECT_._DATASET_.PVT_Conflated` A
 SET A.LandAreaHect = B.Land_Count__Area_in_Hectares_, 
     A.LA22 = B.LTLA22NM
 FROM `_PROJECT_._DATASET_.OA_AREAS` B
 WHERE A.Output_Areas = B.OA21CD;
 
-UPDATE `gdms-demo-20230201.census2021.PVT_Conflated` A
+UPDATE `_PROJECT_._DATASET_.PVT_Conflated` A
 SET A.centroid = B.centroid
 FROM `_PROJECT_._DATASET_.OA21_CENTROIDS` B
 WHERE A.Output_Areas = B.OA21CD;
